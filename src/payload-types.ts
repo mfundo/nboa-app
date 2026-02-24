f9b601dac1e114fd94cc6622a6146158c9684094
Nick Nyamhondoro <nicknyamhondoro@Nicks-MBP.lan>
2026-02-24 21:22:57 +0200

    Fixed merge conflicts

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -319,6 +319,7 @@ export interface Media {
     };
     [k: string]: unknown;
   } | null;
+  prefix?: string | null;
   folder?: (number | null) | FolderInterface;
   updatedAt: string;
   createdAt: string;
@@ -1958,6 +1959,7 @@ export interface PostsSelect<T extends boolean = true> {
 export interface MediaSelect<T extends boolean = true> {
   alt?: T;
   caption?: T;
+  prefix?: T;
   folder?: T;
   updatedAt?: T;
   createdAt?: T;
